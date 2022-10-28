57dcfd035583d3066f5891a84dd65c3bdfee6656
eniola <opakunleeniolail2019@gmail.com>
2022-10-28 14:13:14 +0100

    I made some changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.get("/api", (req, res)=>{
+app.get("/", (req, res)=>{
     res.status(200).json({slackUsername: 'opakunleeniolail2019', backend: true, age: 17, bio: "I am a Full stack developer. I'm proficient in HTML, CSS, Bootstrap, javascript, react.js, angular, node.js, git and github"})
 })
 const PORT = process.env.port || 2300
