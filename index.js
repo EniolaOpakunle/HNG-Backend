d840f5b21239197b851b58a7304e8d2ad16ba02c
eniola <opakunleeniolail2019@gmail.com>
2022-11-01 22:03:55 +0100

    I changed my slackUsername

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.get("/", (req, res)=>{
-    res.status(200).json({slackUsername: 'opakunleeniolail2019', backend: true, age: 17, bio: "I am a Full stack developer. I'm proficient in HTML, CSS, Bootstrap, javascript, react.js, angular, node.js, git and github"})
+    res.status(200).json({slackUsername: 'Eniola Opakunle', backend: true, age: 17, bio: "I am a Full stack developer. I'm proficient in HTML, CSS, Bootstrap, javascript, react.js, angular, node.js, git and github"})
 })
 const PORT = process.env.port || 2300
 app.listen(PORT, ()=>{
