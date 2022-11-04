81a9a1cc6e3b57e30a1e23151db337e196120ac8
eniola <opakunleeniolail2019@gmail.com>
2022-11-04 13:50:32 +0100

    I created a post api endpoint

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 require ('dotenv').config();
 const cors = require('cors')
 app.use(cors());
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.get("/", (req, res)=>{
     res.status(200).json({slackUsername: 'Eniola Opakunle', backend: true, age: 17, bio: "I am a Full stack developer. I'm proficient in HTML, CSS, Bootstrap, javascript, react.js, angular, node.js, git and github"})
+})
+app.post("/api", (req, res)=>{
+    let result;
+    let{operation_type, x, y} = req.body
+    let operation = operation_type.toLowerCase()
+    if (operation == 'addition'){
+        result = eval(parseInt(x) + parseInt(y))
+    }else if(operation == 'subtraction'){
+        result = eval (x - y)
+    }else if(operation == 'multiplication'){
+        result = eval(x * y)
+    }else{
+        result;
+    }
+    res.status(200).json({slackUsername:'Eniola Opakunle', result: parseInt(result), operation_type: operation_type , })
 })
 const PORT = process.env.port || 2300
 app.listen(PORT, ()=>{
